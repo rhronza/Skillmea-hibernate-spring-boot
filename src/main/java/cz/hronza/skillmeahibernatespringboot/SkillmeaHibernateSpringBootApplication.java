0f98e6c502d05ededd4aca5e34ceb9d27e224077
Roman Hronza <roman.hronza@gmail.com>
2023-02-22 10:18:07 +0100

    init commit

---
+++ src/main/java/cz/hronza/skillmeahibernatespringboot/SkillmeaHibernateSpringBootApplication.java
@@ -0,0 +1,13 @@
+package cz.hronza.skillmeahibernatespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillmeaHibernateSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillmeaHibernateSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/hronza/skillmeahibernatespringboot/SkillmeaHibernateSpringBootApplicationTests.java
@@ -0,0 +1,15 @@
+package cz.hronza.skillmeahibernatespringboot;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillmeaHibernateSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
