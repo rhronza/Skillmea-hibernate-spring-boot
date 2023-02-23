3297ae9a421423a7c00a8e68871c1cd2e9d3fe91
Roman Hronza <roman.hronza@gmail.com>
2023-02-23 11:27:31 +0100

    rename to Main.class

--- src/main/java/cz/hronza/skillmeahibernatespringboot/Main.java
+++ src/main/java/cz/hronza/skillmeahibernatespringboot/Main.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SkillmeaHibernateSpringBootApplication {
+public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(SkillmeaHibernateSpringBootApplication.class, args);
+        SpringApplication.run(Main.class, args);
+
     }
 
 }
--- src/test/java/cz/hronza/skillmeahibernatespringboot/MainTests.java
+++ src/test/java/cz/hronza/skillmeahibernatespringboot/MainTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SkillmeaHibernateSpringBootApplicationTests {
+class MainTests {
 
     @Test
     void contextLoads() {
