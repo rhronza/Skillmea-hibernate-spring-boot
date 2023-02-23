fa56ac5fe1946a73d780650f5be0b9237c90b641
Roman Hronza <roman.hronza@gmail.com>
2023-02-23 11:37:13 +0100

    add logger to Main class

--- src/main/java/cz/hronza/skillmeahibernatespringboot/Main.java
+++ src/main/java/cz/hronza/skillmeahibernatespringboot/Main.java
@@ -1,11 +1,13 @@
 package cz.hronza.skillmeahibernatespringboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
-
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
 
